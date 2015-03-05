08e8fd08ffb24bc46bd7e9d68d1bf3f35c419b7f
Michelle <git@shellmultimedia.com>
2015-03-05 15:19:14 -0600

    More moving procedural to OOP and cleanup.

---
+++ src/FileFieldPathsManager.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Drupal\filefield_paths;
+
+use Drupal\file\Entity\File;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Config\Entity\ThirdPartySettingsInterface;
+
+class FileFieldPathsManager {
+  protected $stringProcessor;
+  protected $contentEntity;
+  protected $fieldPathSettings;
+
+  public function __construct(FileFieldPathsStringProcessor $processor) {
+    // The processor handles cleaning up strings and token replacement.
+    $this->stringProcessor = $processor;
+  }
+
+  public function setContentEntity(ContentEntityInterface $entity) {
+    $this->contentEntity = $entity;
+  }
+
+  protected function setFieldPathSettings(array $settings) {
+    $this->fieldPathSettings = $settings;
+  }
+
+  public function processContentEntity() {
+    if ($this->contentEntity instanceof ContentEntityInterface) {
+      // Get a list of the types of fields that have files. (File, integer, video)
+      $field_types = _filefield_paths_get_field_types();
+
+      // Get a list of the fields on this entity.
+      $fields = $this->contentEntity->getFields();
+
+      // Iterate through all the fields looking for ones in our list.
+      foreach ($fields as $key => $field) {
+        // Get the field definition which holds the type and our settings.
+        $field_info = $field->getFieldDefinition();
+
+        // Get the field type, ie: file.
+        $field_type = $field_info->getType();
+
+        // Check the field type against our list of fields.
+        if (isset($field_type) && in_array($field_type, array_keys($field_types))) {
+          $this->processField($field_info);
+        }
+      }
+    }
+  }
+
+  public function processField(ThirdPartySettingsInterface $field_info) {
+    // Retrieve the settings we added to the field.
+    $this->setFieldPathSettings($field_info->getThirdPartySettings('filefield_paths'));
+
+    // If FFP is enabled on this field, process it.
+    if ($this->fieldPathSettings['enabled']) {
+
+      // Get the machine name of the field.
+      $field_name = $field_info->field_name;
+
+      // Go through each item on the field.
+      foreach ($this->contentEntity->{$field_name} as $item) {
+        // Get the file entity associated with the item.
+        $file_entity = $item->entity;
+
+        // Process the file.
+        $this->processFile($file_entity);
+      }
+    }
+  }
+
+  public function processFile($file_entity) {
+    // Retrieve the path/name strings with the tokens from settings.
+    $tokenized_path = $this->fieldPathSettings['filepath'];
+    $tokenized_filename = $this->fieldPathSettings['filename'];
+
+    // Replace tokens.
+    $entity_type = $this->contentEntity->getEntityTypeId();
+    $data = array($entity_type => $this->contentEntity, 'file' => $file_entity);
+    $path = $this->stringProcessor->tokenReplace($tokenized_path, $data);
+    $filename = $this->stringProcessor->tokenReplace($tokenized_filename, $data);
+
+    // Clean with PathAuto.
+    if ($this->fieldPathSettings['path_options']['pathauto_path']) {
+      $path_segments = explode("/", $path);
+      $cleaned_segments = array();
+      foreach ($path_segments as $segment) {
+        $cleaned_segments[] = $this->stringProcessor->pathAutoClean($segment);
+      }
+      $path = implode("/", $cleaned_segments);
+    }
+
+    if ($this->fieldPathSettings['name_options']['pathauto_filename']) {
+      $name_parts = pathinfo($filename);
+      $cleaned_base = $this->stringProcessor->pathAutoClean($name_parts['filename']);
+      $cleaned_extension = $this->stringProcessor->pathAutoClean($name_parts['extension']);
+
+      $filename = $cleaned_base . '.' . $cleaned_extension;
+    }
+
+    // Transliterate.
+    // @TODO: Make transliteration work.
+    if (!$this->fieldPathSettings['name_options']['transliterate_path']) {
+    }
+    if (!$this->fieldPathSettings['name_options']['transliterate_filename']) {
+    }
+
+    // Move the file to its new home.
+    // @TODO: This is failing on the directory creation with permission denied.
+    file_prepare_directory($path, FILE_CREATE_DIRECTORY);
+    file_move($file_entity, 'public://' . $path . '/' . $filename);
+  }
+
+}
